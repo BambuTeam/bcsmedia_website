d1a117bdc0a0ce313176e5d2ce4066291ce464bb
Luis Concua <econtreras@bcs-media.com>
2020-01-24 12:24:01 -0600

    no message

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,19 +3,22 @@ import React, { Component } from "react";
 import img_service_1 from "../assets/image/bcs-media/home-3-service-1.png";
 import img_service_2 from "../assets/image/bcs-media/home-3-service-2.png";
 import img_service_3 from "../assets/image/bcs-media/home-3-service-3.png";
-
+import img_service_4 from "../assets/image/bcs-media/section.png";
 export class Services extends Component {
   render() {
     return (
       <div className="section-padding service-3-bgi" id="features">
         <div className="container service-3-bg">
+          <img src={img_service_4} className="img-section" alt="" width="70px" />
           <div className="row justify-content-center">
             <div className="col-lg-7 text-center">
               <div
                 className="home-3-section-title wow fadeInUp"
                 data-wow-delay="0.2s"
                 data-wow-duration="1s"
+
               >
+
                 <h2>
                   We determined that you're our <strong>target audience</strong>
                 </h2>

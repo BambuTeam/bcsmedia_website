5b0061b1b1240051c4da80f5ae94aa2797eee771
Luis Enrique Concuá <hola@bambu.team>
2021-05-04 14:30:13 -0600

    Shape Position

--- src/components/Shape.js
+++ src/components/Shape.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import Shapeimg from "../assets/image/bcs-media/shape.svg";
 
+
 export class Shape extends Component {
   render() {
     return (
       
-
-      <div className="shapeimg">
+        
+        <div className="shapeimg">
       <img src={Shapeimg} alt="" width="70px" />
 
       </div>
+    
+     
     
     
     );

6b14d228888f0a5331b43e043d91142bf3b45886
Luis Enrique Concuá <hola@bambu.team>
2021-05-04 14:37:40 -0600

    https

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function Main() {
     <Fragment>
       {/* <Header /> */}
       <ReactPlayer
-        url='http://crossmediaplus.com/s3/bcsvideos/Farmacia.MP4'
+        url='https://crossmediaplus.com/s3/bcsvideos/Farmacia.MP4'
         width='100%'
         height='100%'
         playing
